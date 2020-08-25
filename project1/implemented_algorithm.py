ef611dffeb19fa338eff7ca12f3095639cddc251
jelarsso <jelarsso@mail.uio.no>
2020-08-25 20:51:11 +0200

    P1: Algorithm for task b) implemented in python

---
+++ project1/implemented_algorithm.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+n=10000
+h = 1/(n+1)
+
+a = np.zeros((n-1,))-1
+b = np.zeros((n,))+2
+c = np.zeros((n-1,))-1
+
+d = np.zeros((n,))
+l = np.zeros((n-1,))
+
+
+x = np.linspace(0,1,n)#np.zeros((n,))
+f = 100*np.exp(-10*x)*h**2
+sol = 1 - (1-np.exp(-10))*x-np.exp(-10*x)
+
+
+#set f
+
+
+#find LU-decomp
+d[0] = b[0]
+
+for i in range(1,n):
+    l[i-1] = a[i-1]/d[i-1]
+    d[i] = b[i-1] - l[i-1]*c[i-1]
+
+
+"""
+L = np.zeros((n,n))
+U = np.zeros((n,n))
+
+for i in range(n):
+    L[i,i] = 1
+    U[i,i] = d[i]
+
+for i in range(n-1):
+    L[i+1,i] = l[i]
+    U[i,i+1] = c[i]
+
+print(L,"\n\n",U)
+print("\n\n", np.matmul(L,U))
+"""
+
+#find u:
+u = np.zeros((n,))
+v = np.zeros((n,))
+
+u[0] = f[0]
+
+for i in range(1,n):
+    u[i] = f[i] - l[i-1]*u[i-1]
+
+
+u[-1] = u[-1]/d[-1]
+v[-1] = u[-1]
+
+for i in range(n-2,1,-1):
+    v[i] = (u[i] - v[i+1]*c[i])/d[i]
+
+import matplotlib.pyplot as plt
+plt.plot(v)
+plt.plot(sol)
+plt.legend(["algo","anal"])
+plt.show()
+
+print(np.sum(np.abs(sol-v)))
