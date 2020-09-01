408f937df6a5f46210b8c9c2adf429a055f74f6a
jelarsso <jelarsso@mail.uio.no>
2020-09-01 11:48:51 +0200

    Continuing on armadillo implement

--- project1/armadillo_algorithm.cpp
+++ project1/armadillo_algorithm.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <armadillo>
+
 
 using namespace std;
 using namespace arma;
 
-int main(){
-  mat A = randu<mat>(4,5);
-  mat B = randu<mat>(4,5);
-  
-  cout << A*B.t() << endl;
-  
+int main(int argv, char* argc[]){
+
+
+  int n = atoi(argc[0]);
+  mat A = Mat<double>(n,n,fill::zeros);
+  vec b = vector<double>(n);
+
+  mat L,U;
+
+  lu(L,U,A);
+
+  vec u = solve(L,b);
+  vec v = solve(U,u);
+
+
   return 0;
 }
