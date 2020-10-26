da852834bd147d2bff34b45791cbe98ab19920e9
jelarsso <jelarsso@mail.uio.no>
2020-10-26 19:36:42 +0100

    Getters

--- project3/test_solar_system.cpp
+++ project3/test_solar_system.cpp
@@ -5,8 +5,14 @@
 #include<cmath>
 
 TEST_CASE() {
+    //Test that the intial conditions has the correct dimensionalities and values.
+    int *indices;
+    indices = new int[2];
+    indices[0] = 3;
+    indices[1] = 5;
 
-
-
-    
+    SolarSystem sol(3,2,indices,2,false,true);
+    arma::Mat<double> pos = sol.get_init_pos();
+    arma::Mat<double> vel = sol.get_init_vel();
+            
 };
