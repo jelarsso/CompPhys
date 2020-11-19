2343ca9f30ba0e6f0b7075badadf382b524bdb83
Ole Petter <olepma@uio.no>
2020-11-19 13:37:03 +0100

    kjr

--- project4/ising_testing.cpp
+++ project4/ising_testing.cpp
@@ -3,8 +3,8 @@
 int main(){
 
     IsingModel mdl(20, "output1.data");
-    for (int N=1; N<101; N++) {
-        mdl.Metropolis(N,1);
+    for (int N=1; N<1e4+1; N+=100) {
+        mdl.Metropolis(N,0.1);
     }
     return 0;
 }
--- project4/plotting_analytical.py
+++ project4/plotting_analytical.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import numpy as np
 
-N = np.linspace(1, 100, 100)
+N = np.linspace(1, 1e2, int(1e2))
 print(N)
 
 f = open("output1.data", "r")
@@ -9,14 +9,15 @@ f.readline(); f.readline()
 
 E = np.zeros_like(N)
 M = np.zeros_like(N)
-for i in range(90):
+for i in range(int(1e2)):
     sent = f.readline()
     words = sent.split()
     E[i] = words[1]
     M[i] = words[3]
 
 fig = go.Figure(data = go.Scatter(x=N, y=(E)))
-#fig = go.Figure(data = go.Scatter(x=N, y=(M)))
+fig.show()
+fig = go.Figure(data = go.Scatter(x=N, y=(M)))
 fig.show()
 
 #fig.write_image("fig1.pdf")
